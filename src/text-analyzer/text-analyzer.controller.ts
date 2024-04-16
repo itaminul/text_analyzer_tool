d9b204b6e7595b31d937634c0029d0cf3341e477
itaminul <itaminul@gmail.com>
2024-04-17 00:21:22 +0600

    text analyzer count words controller service

--- src/text-analyzer/text-analyzer.controller.ts
+++ src/text-analyzer/text-analyzer.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpStatus } from '@nestjs/common';
+import { TextAnalyzerService } from './text-analyzer.service';
 
 @Controller('text-analyzer')
-export class TextAnalyzerController {}
+export class TextAnalyzerController {
+  constructor(private readonly textAnalyserService: TextAnalyzerService) {}
+  @Get('words')
+  async countWords() {
+    try {
+      const results = await this.textAnalyserService.countWords();
+      return {
+        success: true,
+        status: HttpStatus.OK,
+        results: `Total ${results} Words`,
+      };
+    } catch (error) {
+      return { success: false, message: error.message };
+    }
+  }
+}
--- src/text-analyzer/text-analyzer.service.ts
+++ src/text-analyzer/text-analyzer.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import * as fs from 'fs/promises';
+import { join } from 'path';
 
 @Injectable()
-export class TextAnalyzerService {}
+export class TextAnalyzerService {
+  private readonly sampleFilePath: string;
+
+  constructor() {
+    this.sampleFilePath = join(process.cwd(), 'src', 'sample.txt');
+  }
+
+  async countWords() {
+    try {
+      const words = await fs.readFile(this.sampleFilePath, 'utf-8');
+      return words.split(/\s+/).length;
+    } catch (error) {
+      if (error.code === 'ENOENT') {
+        throw new NotFoundException('Sample file not found.');
+      }
+      throw new Error(`Error reading file: ${error.message}`);
+    }
+  }
+}
