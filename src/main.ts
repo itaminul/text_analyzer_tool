7c5f61bf5161103a954a5b5be14ee3b18e84753d
itaminul@gmail.com <itaminul@gmail.com>
2024-04-17 15:58:50 +0600

    create prisma service and modoule

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const APP_PORT = process.env.APP_PORT || 9007;
   app.setGlobalPrefix('api');
     app.enableCors();
-  await app.listen(3000);
+    await app.listen(APP_PORT, () => {
+      console.info(`Server is listening on port ${APP_PORT}`);
+    });
 }
 bootstrap();
