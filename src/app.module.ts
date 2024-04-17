c9d249db1a7df1ea46b5db5310b6e0bc96c09a30
itaminul@gmail.com <itaminul@gmail.com>
2024-04-17 17:58:59 +0600

    total-words-count-done-with-service-controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,13 @@ import { TextAnalyzerModule } from './text-analyzer/text-analyzer.module';
 import { TextAnalyzerController } from './text-analyzer/text-analyzer.controller';
 import { TextAnalyzerService } from './text-analyzer/text-analyzer.service';
 import { DatabaseTextAnalyzerModule } from './database-text-analyzer/database-text-analyzer.module';
+import { DatabaseTextAnalyzerController } from './database-text-analyzer/database-text-analyzer.controller';
+import { DatabaseTextAnalyzerService } from './database-text-analyzer/database-text-analyzer.service';
+import { PrismaModule } from './database/prisma/prisma.module';
 
 @Module({
-  imports: [TextAnalyzerModule, DatabaseTextAnalyzerModule],
-  controllers: [TextAnalyzerController],
-  providers: [TextAnalyzerService],
+  imports: [PrismaModule, TextAnalyzerModule, DatabaseTextAnalyzerModule],
+  controllers: [TextAnalyzerController, DatabaseTextAnalyzerController],
+  providers: [TextAnalyzerService, DatabaseTextAnalyzerService],
 })
 export class AppModule {}
--- src/database-text-analyzer/database-text-analyzer.service.ts
+++ src/database-text-analyzer/database-text-analyzer.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../database/prisma/prisma.service';
 
 @Injectable()
-export class DatabaseTextAnalyzerService {}
+export class DatabaseTextAnalyzerService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async getTotalWords(): Promise<number> {
+    const sampleWords = await this.prismaService.sample.findMany({
+      where: {
+        activeStatus: true,
+      },
+      select: {
+        sampleText: true,
+      },
+    });
+
+    let totalWords = 0;
+    for (const sampleWord of sampleWords) {
+      totalWords += this.countWords(sampleWord.sampleText);
+    }
+    return totalWords;
+  }
+
+  private countWords(text: string): number {
+    return text.split(/\s+/).filter((word) => word !== '').length;
+  }
+}
