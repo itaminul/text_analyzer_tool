1218f84ed7caff298cfaab10c2a1b225bd4a4ce4
itaminul@gmail.com <itaminul@gmail.com>
2024-04-17 18:28:36 +0600

    unit-testing-total-words-count-done-with-service-controller

--- src/database-text-analyzer/database-text-analyzer.controller.spec.ts
+++ src/database-text-analyzer/database-text-analyzer.controller.spec.ts
@@ -1,15 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DatabaseTextAnalyzerController } from './database-text-analyzer.controller';
-
+import { DatabaseTextAnalyzerService } from './database-text-analyzer.service';
+import { PrismaService } from '../database/prisma/prisma.service';
+class MockPrismaService {}
 describe('DatabaseTextAnalyzerController', () => {
   let controller: DatabaseTextAnalyzerController;
+  let service: DatabaseTextAnalyzerService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [DatabaseTextAnalyzerController],
+      providers: [
+        DatabaseTextAnalyzerService,
+        { provide: PrismaService, useClass: MockPrismaService },
+      ],
     }).compile();
 
-    controller = module.get<DatabaseTextAnalyzerController>(DatabaseTextAnalyzerController);
+    controller = module.get<DatabaseTextAnalyzerController>(
+      DatabaseTextAnalyzerController,
+    );
+    service = module.get<DatabaseTextAnalyzerService>(
+      DatabaseTextAnalyzerService,
+    );
   });
 
   it('should be defined', () => {
