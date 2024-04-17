28cce7f24594675b168168bd6769146243ec33e1
itaminul@gmail.com <itaminul@gmail.com>
2024-04-17 11:47:23 +0600

    characters count done with service controller

--- src/text-analyzer/text-analyzer.service.ts
+++ src/text-analyzer/text-analyzer.service.ts
@@ -13,7 +13,20 @@ export class TextAnalyzerService {
   async countWords() {
     try {
       const words = await fs.readFile(this.sampleFilePath, 'utf-8');
-      const results =  words.split(/\s+/).length;
+      const results = words.split(/\s+/).length;
+      return results;
+    } catch (error) {
+      if (error.code === 'ENOENT') {
+        throw new NotFoundException('Sample file not found.');
+      }
+      throw new Error(`Error reading file: ${error.message}`);
+    }
+  }
+
+  async countCharacters() {
+    try {
+      const text = await fs.readFile(this.sampleFilePath, 'utf-8');
+      const results = text.length;
       return results;
     } catch (error) {
       if (error.code === 'ENOENT') {
